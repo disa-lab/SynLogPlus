8e2c0fca2e92e58df4d0bf08b4bebff02042a76b
Enan Ajmain <3nan.ajmain@gmail.com>
2025-05-12 23:13:47 -0400

    Fix LogMine evaluator script

--- benchmark/evaluation/LogMine_eval.py
+++ benchmark/evaluation/LogMine_eval.py
@@ -78,7 +78,6 @@ if __name__ == "__main__":
     else:
         datasets = datasets_2k
     for dataset in datasets:
-        if dataset != 'HPC': continue
         setting = benchmark_settings[dataset]
         log_file = setting['log_file'].replace("_2k", f"_{data_type}")
         indir = os.path.join(input_dir, os.path.dirname(log_file))
